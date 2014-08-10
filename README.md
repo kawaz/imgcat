d6ca1c1627779162cb9c1fe478c9df43c771f233
Yoshiaki Kawazu <kawazzz@gmail.com>
2014-08-11 02:31:44 +0900

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ imgcat
 ======
 
 cat image to terminal
+
+# Install
+
+```bash:
+# for Mac
+PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig npm install -g imgcat
+```
+
---
+++ imgcat.js
@@ -0,0 +1,28 @@
+
+var tc = require('./node_modules/ansi-canvas/');
+var Canvas = require('canvas');
+
+// create terminal <canvas>
+var canvas = tc();
+var ctx = canvas.getContext('2d');
+
+var screenWidth = canvas.width;
+var maxWidth = parseInt(process.argv[3], 10) || screenWidth;
+
+var imageFile = process.argv[2];
+var image = require('fs').readFileSync(imageFile);
+var img = new Canvas.Image();
+img.src = image;
+
+
+//console.log([img, typeof img, img instanceof Canvas.Image]);process.exit(0);
+
+
+var width = maxWidth;
+var scaleW = img.width > width ? width / img.width : 1;
+var w = Math.floor(img.width * scaleW);
+var h = Math.floor(img.height * scaleW);
+
+ctx.drawImage(img, 0, 0, w, h);
+
+canvas.render();
