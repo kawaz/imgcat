09d4badac6975696855bb40774c850a5730528ce
Yoshiaki Kawazu <kawazzz@gmail.com>
2014-08-11 14:59:32 +0900

    画像が画面に収まるように調整するようにした

--- imgcat.js
+++ imgcat.js
@@ -8,22 +8,16 @@ var Image = Canvas.Image;
 var canvas = tc();
 var ctx = canvas.getContext('2d');
 
-var screenWidth = canvas.width;
-var maxWidth = parseInt(process.argv[3], 10) || screenWidth;
-
 var imageFile = process.argv[2];
 var image = require('fs').readFileSync(imageFile);
 var img = new Image();
 img.src = image;
 
+var scale = 1 / Math.max(img.width/canvas.width, img.height/canvas.height);
+var w = Math.floor(img.width * scale);
+var h = Math.floor(img.height * scale);
 
-//console.log([img, typeof img, img instanceof Canvas.Image]);process.exit(0);
-
-
-var width = maxWidth;
-var scaleW = img.width > width ? width / img.width : 1;
-var w = Math.floor(img.width * scaleW);
-var h = Math.floor(img.height * scaleW);
+//console.log([img, typeof img, img instanceof Canvas.Image]); process.exit(0);
 
 ctx.drawImage(img, 0, 0, w, h);
 
